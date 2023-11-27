92d5233fa91f62365492f3eb25ea34eb6b4bbbab
toannv72 <64313624+toannv72@users.noreply.github.com>
2023-11-28 01:32:04 +0700

    up

--- src/Components/BirthdayCard.jsx
+++ src/Components/BirthdayCard.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './BirthdayCard.css'; 
 import { Button } from 'antd';
 import { useNavigate } from 'react-router-dom';
+import { TypeAnimation } from 'react-type-animation';
 
 const BirthdayCard = () => {
   const navigate = useNavigate();
@@ -16,7 +17,18 @@ const BirthdayCard = () => {
         <div><span className='text'>Y</span></div>
         <div><span className='text'>!</span></div>
       </div>
-      <h1>Sắp đến sinh nhật tui rồi 17/12 cho nên là...</h1>
+      <TypeAnimation
+                    className='text-h1'
+                    sequence={[
+                        // Same substring at the start will only be typed once, initially
+                        `Sắp đến sinh nhật tui rồi 17/12 cho nên là...`,
+                        1000,
+                       
+                    ]}
+                    speed={50}
+                    style={{ fontSize: '3em',  whiteSpace: 'pre-line', display: 'block' }}
+                // repeat={Infinity}
+                />
       <div className='button'><Button onClick={()=>navigate('/page2')}  >Tiếp tục</Button></div>
 
     </div>
--- src/page/SignInWithGoogle.jsx
+++ src/page/SignInWithGoogle.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { signInWithPopup, GoogleAuthProvider, onAuthStateChanged } from 'firebase/auth';
 import GoogleButton from "react-google-button";
 import { auth } from '../configs/firebase';
+import { TypeAnimation } from 'react-type-animation';
 
 const SignInWithGoogle = ({ setLoading,
     loading, }) => {
@@ -23,24 +24,36 @@ const SignInWithGoogle = ({ setLoading,
             .finally(() => {
                 setLoading(!loading);
             });
-        
+
 
     };
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-          localStorage.setItem("loginG", JSON.stringify(currentUser));
+            localStorage.setItem("loginG", JSON.stringify(currentUser));
         });
         return () => {
-          unsubscribe();
+            unsubscribe();
         };
         // eslint-disable-next-line react-hooks/exhaustive-deps
-      }, []);
+    }, []);
     return (
         <div className='flex flex-col justify-center items-center'>
             Vui lòng đăng nhập để tiếp tục
             <GoogleButton onClick={googleSignIn} color="blue">
                 Sign In with Google
             </GoogleButton>
+            <TypeAnimation
+                className=''
+                sequence={[
+                    // Same substring at the start will only be typed once, initially
+                    `Đăng nhập không mất tài khoản đâu mà lo 😢😢😢....`,
+                    1000,
+
+                ]}
+                speed={50}
+                style={{ fontSize: '3em', whiteSpace: 'pre-line', display: 'block' }}
+            // repeat={Infinity}
+            />
         </div>
 
     );
